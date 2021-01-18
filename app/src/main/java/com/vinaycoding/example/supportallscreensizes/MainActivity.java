94c9fe6d75ac5003744ea4163d3af49ca037ce52
vinay suresh <vinay.suresh@girmiti.com>
2021-01-18 13:30:55 +0530

    Support screen for multiple devices and tablets.

---
+++ app/src/main/java/com/vinaycoding/example/supportallscreensizes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vinaycoding.example.supportallscreensizes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
